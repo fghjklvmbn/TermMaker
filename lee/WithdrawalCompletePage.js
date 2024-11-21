cb0bbd85fd60ee8ffa5fa6c7f3b355be5d6d4811
20221923 <lpl125@naver.com>
2024-11-21 20:12:25 +0900

    Create WithdrawalCompletePage.js

---
+++ lee/WithdrawalCompletePage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+const WithdrawalCompletePage = ({ navigation }) => {
+    const handleConfirm = () => {
+        navigation.goBack(); // 이전 페이지로 돌아가기
+    };
+
+    return (
+        <View style={styles.container}>
+            {/* 회원탈퇴 텍스트 */}
+            <Text style={styles.title}>회원탈퇴</Text>
+
+            {/* 회원탈퇴 완료 텍스트 */}
+            <Text style={styles.subtitle}>회원탈퇴 되었습니다.</Text>
+
+            {/* 확인 버튼 */}
+            <TouchableOpacity style={styles.button} onPress={handleConfirm}>
+                <Text style={styles.buttonText}>확인</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+        padding: 20,
+    },
+    title: {
+        fontSize: 33, // 기존보다 크게
+        fontWeight: 'bold',
+        marginBottom: 105,
+      },
+    subtitle: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#3737A0',
+        marginBottom: 50,
+        textAlign: 'center',
+    },
+    button: {
+        backgroundColor: '#D9D9D9',
+        paddingVertical: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+        width: '60%',
+    },
+    buttonText: {
+        color: '#000',
+        fontSize: 16,
+    },
+});
+
+export default WithdrawalCompletePage;
