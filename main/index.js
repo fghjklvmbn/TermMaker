ae36e9606b41c9c1e282f667ddb5d6d383472469
20221923 <lpl125@naver.com>
2024-12-04 15:46:56 +0900

    Create index.js

---
+++ main/index.js
@@ -0,0 +1,31 @@
+// index.js
+
+// 기본 페이지
+export { default as Mypage } from "./mypage";
+export { default as PersonalInfoPage } from "./PersonalInfoEditPage";
+export { default as WithdrawalPage } from "./WithdrawalPage";
+export { default as WithdrawalErrorPage } from "./WithdrawalErrorPage";
+export { default as FinalWithdrawalPage } from "./FinalWithdrawalPage";
+export { default as WithdrawalCompletePage } from "./WithdrawalCompletePage";
+
+// 결제수단 페이지
+export { default as PaymentMethodsPage01 } from "./PaymentMethodsPage01";
+export { default as PaymentMethodsPage02 } from "./PaymentMethodsPage02";
+export { default as PaymentMethodsPage03 } from "./PaymentMethodsPage03";
+
+// 프로필 관련 페이지
+export { default as ProfileEditPage } from "./ProfileEditPage";
+export { default as ProfileEditPage02 } from "./ProfileEditPage02";
+export { default as PersonalInfoEditPage } from "./PersonalInfoEditPage";
+
+// 기타 페이지
+export { default as LogoutSuccessPage } from "./SignupSuccessPage";
+export { default as SignupSuccessPage } from "./SignupSuccessPage";
+export { default as AccountRecoveryPage } from "./AccountRecoveryPage";
+export { default as MainScreen } from "./MainScreen";
+export { default as FindIDPage } from "./FindIDPage";
+export { default as FindIDResultPage } from "./FindIDResultPage";
+export { default as FindPasswordPage } from "./FindPasswordPage";
+export { default as FindPasswordResultPage } from "./FindPasswordResultPage";
+export { default as AccountNotFoundPage } from "./AccountNotFoundPage";
+export { default as AutoLoginPage } from "./AutoLoginPage";
