8ceb16851a27c404e1789d6dd973b64cc9022a7b
park <dsapoi881@gmail.com>
2024-12-12 14:42:01 +0900

    백엔드 수정

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.listen(PORT, () => {
 
 // 각 기능 라우터 등록
 app.use('/api/payment', paymentRoutes); // 결제수단 관련 요청
-app.use('/api/community', communityRoutes); // 커뮤니티 관련 요청
+app.use('/api/board', communityRoutes); // 커뮤니티 관련 요청
 app.use('/api/comment', commentRoutes); // 댓글 관련 요청
 app.use('/api/wheelchair', wheelchairRoutes); // 휠체어 관련 요청
 app.use('/api/user', userRoutes)  // 로그인/회원가입 등
